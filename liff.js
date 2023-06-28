b4a6b9b65815fa50de1583f48fcee1c052db41c5
ShuSakiyama <75059253+ShuSakiyama@users.noreply.github.com>
2023-06-28 18:43:40 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
     // LINE DevelopersのLIFF画面より確認可能
-    var liffId = "2000000793-ZGYyvvKJ";
+    var liffId = "2000000793-NqP9ooZQ";
     console.log(`init liff, ID : ${liffId}`);
     initializeLiff(liffId);
 })
